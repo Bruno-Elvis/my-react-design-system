c41b02e3e96da04514d25f264ec5c128fed47382
Bruno-Elvis <brunoelvis@outlook.com.br>
2022-10-18 20:15:06 -0300

    config CI storybook + layouts changes

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,16 +3,28 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
+
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
   },
+
   "features": {
     "storyStoreV7": true
+  },
+
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/my-react-design-system/';
+    };
+
+    return config;
   }
+
 }
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,7 +5,7 @@ export interface CheckboxProps {  };
 
 export function Checkbox(props: CheckboxProps) {
     return (
-        <CheckboxPrimitive.Root className='w-6 h-6 p-[2px] bg-stone-800 rounded focus:ring-2 ring-stone-50'>
+        <CheckboxPrimitive.Root className='w-6 h-6 p-[2px] bg-stone-800 rounded focus:ring-2 ring-stone-50 outline-none'>
             <CheckboxPrimitive.Indicator>
                 <Check weight='bold' className='h-5 w-5 text-stone-50' />
             </CheckboxPrimitive.Indicator>
